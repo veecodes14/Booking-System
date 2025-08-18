8c222bd12c5b60c81ebd07e31ef771012597c93d
veecodes14 <134261808+veecodes14@users.noreply.github.com>
2025-08-18 21:13:07 +0000

    serializers created

---
+++ bookings/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import CustomerProfile, ServiceProvider, Service, AvailabilityRule, Blackout, Booking
+
+class CustomerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerProfile
+        fields=  ['id', 'user', 'created_at', 'updated_at', 'default_location', 'notes']
+
+class ServiceProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceProvider
+        fields = ['id', 'user', 'display_name', 'bio', 'business_name', 'address', 'avg_rating', 'google_connection']
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['id', 'provider', 'title', 'description', 'duration_minutes', 'price', 'category', 'buffer_before', 'buffer_after']
+
+class AvailabilityRuleSerialicer(serializers.ModelSerializer):
+    class Meta:
+        model = AvailabilityRule
+        fields = ['id', 'provider', 'day_of_week', 'start_time', 'end_time', 'is_active']
+
+class Blackout(serializers.ModelSerializer):
+    class Meta:
+        model = Blackout
+        fields = ['id', 'provider', 'start_datetime', 'end_datetime', 'reason']
+
+class Booking(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'customer', 'provider', 'service', 'start_datetime_utc', 'end_datetime_utc', 'price_snapshot', 'status', 'notes']
+
+
